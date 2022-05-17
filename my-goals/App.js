3303d8cf3cfee77078a0385968b6b94082271a67
Muhammad Hamza <muhammadhamza.smhn@gmail.com>
2022-05-17 15:20:33 +0500

    styled goal item
    24

--- my-goals/App.js
+++ my-goals/App.js
@@ -48,7 +48,9 @@ export default function App() {
       </View>
       <View style={styles.goalsContainer}>
         {courseGoals.map((goal) => (
-          <Text key={goal}>{goal}</Text>
+          <View key={goal} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
         ))}
       </View>
     </View>
@@ -79,5 +81,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
   },
 });
