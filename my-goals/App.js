5a90a862ece99e2cedaffd81ae7f0452d010f891
Muhammad Hamza <muhammadhamza.smhn@gmail.com>
2022-05-17 17:54:15 +0500

    made deletable item using ids
    31

--- my-goals/App.js
+++ my-goals/App.js
@@ -37,8 +37,10 @@ export default function App() {
     ]);
   }
 
-  function deleteGoalHandler() {
-    console.log("DELETE");
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
@@ -51,6 +53,7 @@ export default function App() {
             return (
               <GoalItem
                 text={itemData.item.text}
+                id={itemData.item.id}
                 onDeleteItem={deleteGoalHandler}
               />
             );
