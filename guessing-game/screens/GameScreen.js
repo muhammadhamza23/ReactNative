014a3de7cd4a6e45aedc5443787b534433a98b75
Muhammad Hamza <muhammadhamza.smhn@gmail.com>
2022-05-20 18:50:46 +0500

    logged game rounds
    74

--- guessing-game/screens/GameScreen.js
+++ guessing-game/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { View, StyleSheet, Alert } from "react-native";
+import { View, StyleSheet, Alert, Text } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import NumberContainer from "../components/game/NumberContainer";
@@ -24,6 +24,7 @@ let upperBound = 100;
 function GameScreen({ userNumber, onGameOver }) {
   const initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  const [guessRounds, setGuessRounds] = useState([initialGuess]);
 
   useEffect(() => {
     if (currentGuess === userNumber) {
@@ -58,6 +59,7 @@ function GameScreen({ userNumber, onGameOver }) {
       currentGuess
     );
     setCurrentGuess(newRndNumber);
+    setGuessRounds((prevGuessRounds) => [newRndNumber, ...prevGuessRounds]);
   }
 
   return (
@@ -83,7 +85,11 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      {/* <View>Round Logs</View> */}
+      <View>
+        {guessRounds.map((guessRound) => (
+          <Text key={guessRound}> {guessRound} </Text>
+        ))}
+      </View>
     </View>
   );
 }
