8c560bb4e637e89054e9f6d5735714e4086a9f67
Muhammad Hamza <muhammadhamza.smhn@gmail.com>
2022-05-23 12:12:07 +0500

    added rounds count and finishing touches
    76-77

--- guessing-game/screens/GameScreen.js
+++ guessing-game/screens/GameScreen.js
@@ -29,7 +29,7 @@ function GameScreen({ userNumber, onGameOver }) {
 
   useEffect(() => {
     if (currentGuess === userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
 
@@ -88,7 +88,7 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      <View>
+      <View style={styles.listContainer}>
         {/* {guessRounds.map((guessRound) => (
           <Text key={guessRound}> {guessRound} </Text>
         ))} */}
@@ -122,5 +122,9 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
   },
 });
