99b9949d1be015d2b3e85b9573ed9e4d16b71c09
Muhammad Hamza <muhammadhamza.smhn@gmail.com>
2022-05-23 12:43:44 +0500

    added rotation during game play
    by using useWindowDimensions API to adjust margin according to the orientation during gameplay
    
    84.2

--- guessing-game/screens/StartGameScreen.js
+++ guessing-game/screens/StartGameScreen.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { TextInput, View, StyleSheet, Alert, Dimensions } from "react-native";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Alert,
+  useWindowDimensions,
+} from "react-native";
 
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
@@ -9,6 +15,8 @@ import InstructionText from "../components/ui/InstructionText";
 
 function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
+
+  const { width, height } = useWindowDimensions("");
 
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -31,8 +39,10 @@ function StartGameScreen({ onPickNumber }) {
     onPickNumber(chosenNumber);
   }
 
+  const marginTopDistance = height < 380 ? 30 : 100;
+
   return (
-    <View style={styles.rootContainer}>
+    <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
       <Title>Guess My Number</Title>
       <Card>
         <InstructionText>Enter a Number</InstructionText>
@@ -60,12 +70,12 @@ function StartGameScreen({ onPickNumber }) {
 
 export default StartGameScreen;
 
-const deviceHeight = Dimensions.get("window").height;
+// const deviceHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    marginTop: deviceHeight < 380 ? 30 : 100,
+    // marginTop: deviceHeight < 380 ? 30 : 100,
     alignItems: "center",
   },
   numberInput: {
